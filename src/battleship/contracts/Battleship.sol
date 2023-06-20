109a1cdc8f274ed227822a18535144b2ee26da02
Nicola Venturi <n.venturi3@studenti.unipi.it>
2023-06-20 14:43:27 +0200

    First commit

---
+++ src/battleship/contracts/Battleship.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Battleship {
+  address[20] public games;
+
+  function play(uint256 gameId) public returns (uint) {
+    require(gameId>=0 && gameId<=20);
+
+    return gameId;
+  }
+}
